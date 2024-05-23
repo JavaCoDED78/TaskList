cec4338f80dfc7be56e041481250e0e14751153b
Andrei Soroka <131610832+JavaCoDED78@users.noreply.github.com>
2024-05-23 19:54:24 +0300

    [Feature] Implementation of services and repositories, JDBC

--- src/main/java/com/javaded/domain/user/User.java
+++ src/main/java/com/javaded/domain/user/User.java
@@ -15,29 +15,13 @@ import java.util.List;
 import java.util.Set;
 
 @Data
-@Entity
 public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
     private Long id;
-
-    @Column(name = "name", nullable = false)
     private String name;
-
-    @Column(name = "email", nullable = false)
     private String username;
-
-    @Column(name = "password", nullable = false)
     private String password;
-
-    @Column(name = "password_confirmation", nullable = false)
     private String passwordConfirmation;
-
     private Set<Role> roles;
-
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
     private List<Task> tasks;
-
 }
